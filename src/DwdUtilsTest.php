7cedbcdc614fc24983623f0aee04656e07e6e5a4
Martin Schimmel <martin.schimmel@drukwerkdeal.nl>
2016-02-10 20:53:55 +0100

    even moar tests!

--- src/DwdUtilsTest.php
+++ src/DwdUtilsTest.php
@@ -14,26 +14,29 @@ class DwdUtilsTest extends \PHPUnit_Framework_TestCase
     /**
      * @dataProvider provider
      * @param $input
-     * @param $length
-     * @param $trailing
-     * @param $output
+     * @param $expectedOutput
+     * @param $expectedOutput2
+     * @param int $length
+     * @param string $trailing
+     * @internal param $output
      */
-    public function testExcerpt($input, $output, $length = 25, $trailing = '…')
+    public function testExcerpt($input, $expectedOutput, $expectedOutput2, $length = 25, $trailing = '…')
     {
         $utils = new DwdUtils();
 
         $result1 = $utils::excerpt($input, $length, $trailing);
-        $this->assertEquals($result1, $output);
+        $this->assertEquals($expectedOutput, $result1);
+        $this->assertNotEquals($expectedOutput2, $result1);
     }
 
     public function provider()
     {
         return array(
-            "Most basic test with short string" => array("test input", "test input"),
-            "Basic test with long string" => array("test input, but now a lot longer", "test input, but now a…"),
-            "testing length parameter" => array("test input, but now a lot longer", "test…", 5),
-            "testing all params1" => array("test input, but now a lot longer again", "test...", 5, "..."),
-            "testing all params2" => array("test input, but now a lot longer again", "testWAZAAAA!", 5, "WAZAAAA!"),
+            "Most basic test with short string" => array("test input", "test input", "test input'…'"),
+            "Basic test with long string" => array("test input, but now a lot longer", "test input, but now a…", "test input, but now a lot longer"),
+            "testing length parameter" => array("test input, but now a lot longer", "test…", "test input, but now a lot longer", 5),
+            "testing all params1" => array("test input, but now a lot longer again", "test...", "test input, but now a lot longer again", 5, "..."),
+            "testing all params2" => array("test input, but now a lot longer again", "testWAZAAAA!", "test input, but now a lot longer again", 5, "WAZAAAA!"),
         );
     }
 }
